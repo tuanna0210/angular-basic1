86d148081616430bd0460aab3f6d491e65fd9290
Tuan <anhtuann0210@gmail.com>
2019-07-25 21:50:49 +0700

    Routing P2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes : Hero[];
-  selectedHero : Hero;
+  heroes: Hero[];
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
